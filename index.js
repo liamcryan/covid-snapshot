dbef6e7467f6974048d5c1a482d445a6dca5f214
Gogs <gogs@fake.local>
2020-08-13 11:10:20 -0700

    update with line chart

--- index.js
+++ index.js
@@ -5,6 +5,68 @@ Historic values for all states
 */
 
 // very helpful for bar chart: https://observablehq.com/@d3/horizontal-bar-chart
+function toDate (d) {
+  d = d.toString()
+  return new Date(d.slice(0,4), parseInt(d.slice(4,6)) - 1, d.slice(6,8))
+}
+
+
+function lineChart (data, metric, date, states) {
+  // sum metric per date
+  let lineData = []
+  data.reduce((res, d) => {
+    if (!res[d.date]) {
+      res[d.date] = {date: d.date};
+      res[d.date][metric] = 0;
+      lineData.push(res[d.date])
+    }
+    if (states.includes(d.state)) {
+      res[d.date][metric] += d[metric];
+    }
+    return res
+  }, {});
+
+  data = lineData.map(d => {
+    d.properDate = toDate(d.date)
+    return d;
+  })
+
+  let margin = {top: 20, right: 30, bottom: 30, left: 60};
+  const width = 1000;
+  const height = 400;
+
+  const svg = d3.create('svg')
+      .attr('viewBox', [0, 0, width, height]);
+
+  x = d3.scaleTime()
+        .domain(d3.extent(data, d => d.properDate))
+        .range([margin.left, width - margin.right])
+
+  y = d3.scaleLinear()
+        .domain([d3.max(data, d => d[metric]), d3.min(data, d => d[metric])])
+        .range([margin.top, height - margin.bottom])
+
+  let line = d3.line()
+      .x(d => x(d.properDate))
+      .y(d => y(d[metric]));
+
+  svg.append('path')
+    .datum(data)
+      .attr('d', line)
+      .attr('fill', 'none')
+      .attr("stroke", "#000");
+
+  svg.append('g')
+      .attr('transform', `translate(0,${height - margin.top})`)
+    .call(d3.axisBottom(x));
+
+  svg.append('g')
+      .attr('transform', `translate(${margin.left},0)`)
+    .call(d3.axisLeft(y));
+
+  return svg.node();
+}
+
 
 function barChart (data, metric, date, states) {
   data = data.filter(d => (d.date == date && states.includes(d.state)));
@@ -16,7 +78,7 @@ function barChart (data, metric, date, states) {
   let height = Math.ceil((data.length + 0.1) * barHeight) + margin.top + margin.bottom;
 
   x = d3.scaleLinear()
-      .domain([0, d3.max(data, d => d[metric])])
+      .domain([d3.min(data, d => d[metric]), d3.max(data, d => d[metric])])
       .range([margin.left, width - margin.right])
 
   y = d3.scaleBand()
@@ -83,4 +145,7 @@ d3.json('https://api.covidtracking.com/api/v1/states/daily.json')
   .then(data => {
       const barChartDiv = document.getElementById('bar-chart');
       barChartDiv.append(barChart(data, metric, today, states));
+
+      const lineChartDiv = document.getElementById('line-chart');
+      lineChartDiv.append(lineChart(data, metric, today, states));
     });
